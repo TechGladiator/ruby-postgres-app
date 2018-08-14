0706953b883d6a8c1502c5430f60ff8c1af86fd8
John Stratton <jstrat0407@gmail.com>
2018-08-14 10:24:59 -0400

    removed all resources

--- app/models/import.rb
+++
@@ -1,3 +0,0 @@
-class Import < ApplicationRecord
-  has_many :records
-end
--- db/migrate/20180814141109_add_import_to_records.rb
+++
@@ -1,5 +0,0 @@
-class AddImportToRecords < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :records, :import, foreign_key: true
-  end
-end
