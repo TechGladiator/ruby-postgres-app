d28d25cf9c942edab08d085629a4be5c96ef9ac0
John Stratton <jstrat0407@gmail.com>
2018-08-14 10:00:59 -0400

    regenerated import model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_135521) do
+ActiveRecord::Schema.define(version: 2018_08_14_140019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "imports", force: :cascade do |t|
+    t.string "import_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "records", force: :cascade do |t|
     t.string "name"
