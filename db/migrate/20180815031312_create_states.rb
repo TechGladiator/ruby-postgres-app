70225646aea5e844d0edb1f552163a81b22df7de
John Stratton <jstrat0407@gmail.com>
2018-08-14 23:15:23 -0400

    set name and short_name to not null

--- db/migrate/20180815031312_create_states.rb
+++ db/migrate/20180815031312_create_states.rb
@@ -1,8 +1,8 @@
 class CreateStates < ActiveRecord::Migration[5.2]
   def change
     create_table :states do |t|
-      t.string :name
-      t.string :short_name
+      t.string :name, null: false
+      t.string :short_name, null: false
 
       t.timestamps
     end
