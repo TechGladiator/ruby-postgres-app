0a6d04e646261f87a3fbb4288a5398157a6ada65
John Stratton <jstrat0407@gmail.com>
2018-08-14 20:51:52 -0400

    limit short_name to 2 characters

--- db/migrate/20180815004938_create_states.rb
+++ db/migrate/20180815004938_create_states.rb
@@ -2,7 +2,7 @@ class CreateStates < ActiveRecord::Migration[5.2]
   def change
     create_table :states do |t|
       t.string :name, null: false
-      t.string :short_name, null: false
+      t.string :short_name, limit: 2, null: false
 
       t.timestamps
     end
