ace6243ea4ab4d19f94ef550da6795bc1c129b3d
John Stratton <jstrat0407@gmail.com>
2018-08-15 16:55:06 -0400

    rolled back last migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_034852) do
+ActiveRecord::Schema.define(version: 2018_08_15_031312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,30 +22,10 @@ ActiveRecord::Schema.define(version: 2018_08_15_034852) do
     t.index ["import_name"], name: "imports_import_name_key", unique: true
   end
 
-  create_table "records", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "address", null: false
-    t.string "address_2"
-    t.string "city", null: false
-    t.string "state", limit: 2, null: false
-    t.string "zip", null: false
-    t.string "purpose", null: false
-    t.string "property_owner", null: false
-    t.date "creation_date", null: false
-    t.decimal "lat", null: false
-    t.decimal "long", null: false
-    t.bigint "import_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["import_id"], name: "index_records_on_import_id"
-  end
-
   create_table "states", primary_key: "short_name", id: :string, limit: 2, force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "records", "imports"
-  add_foreign_key "records", "states", column: "state", primary_key: "short_name"
 end
