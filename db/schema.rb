d112c6af41bcac10892cf53a56fd6cdcdddf28bd
John Stratton <jstrat0407@gmail.com>
2018-08-14 23:47:01 -0400

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_025746) do
+ActiveRecord::Schema.define(version: 2018_08_15_031312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2018_08_15_025746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["import_name"], name: "imports_import_name_key", unique: true
+  end
+
+  create_table "states", primary_key: "short_name", id: :string, limit: 2, force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
