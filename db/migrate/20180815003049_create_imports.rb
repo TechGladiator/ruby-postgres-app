76fb535b0d48e418dfc1ef370612ac8fc8601c30
John Stratton <jstrat0407@gmail.com>
2018-08-14 20:34:03 -0400

    set import_name to not null

--- db/migrate/20180815003049_create_imports.rb
+++ db/migrate/20180815003049_create_imports.rb
@@ -1,7 +1,7 @@
 class CreateImports < ActiveRecord::Migration[5.2]
   def change
     create_table :imports do |t|
-      t.string :import_name
+      t.string :import_name, null: false
 
       t.timestamps
     end
