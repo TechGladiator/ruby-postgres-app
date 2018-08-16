a3ef5e246658394934103f4fd7e1625fee1168af
John Stratton <jstrat0407@gmail.com>
2018-08-16 15:28:11 -0400

    added routes for records

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   resources :imports
+
+  get    '/records',          to: 'records#index', as: 'records'
+  post   '/records',          to: 'records#create'
+  get    '/records/new',      to: 'records#new',   as: 'new_record'
+  get    '/records/:id',      to: 'records#show',  as: 'record'
+  get    '/records/:id/edit', to: 'records#edit',  as: 'edit_record'
+  patch  '/records/:id',      to: 'records#update'
+  delete '/records/:id',      to: 'records#destroy'
+  
 end
