8232a38720a8f10e17879bb5931f2d041b111fdc
John Stratton <jstrat0407@gmail.com>
2018-08-16 16:34:53 -0400

    setup record controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,12 @@
 class RecordsController < ApplicationController
+  def create
+    @import = Import.find(params[:import_id])
+    @record = @import.records.create(record_params)
+    redirect_to @import
+  end
+
+  private
+    def record_params
+      params.require(:record).permit(:name, :address, :address_2, :city, :state, :zip, :purpose, :property_owner, :creation_date, :lat, :long)
+    end
 end
