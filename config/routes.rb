fa327b29eb1f5809449545aa30d797596a01360e
John Stratton <jstrat0407@gmail.com>
2018-08-16 15:46:07 -0400

    swapped routes with records resource shortcut

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   resources :imports do
-    get    '/records',          to: 'records#index', as: 'records'
-    post   '/records',          to: 'records#create'
-    get    '/records/new',      to: 'records#new',   as: 'new_record'
-    get    '/records/:id',      to: 'records#show',  as: 'record'
-    get    '/records/:id/edit', to: 'records#edit',  as: 'edit_record'
-    patch  '/records/:id',      to: 'records#update'
-    delete '/records/:id',      to: 'records#destroy'
+    resources :records
   end
 
 end
